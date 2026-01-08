e583ee844806126f1550e3c690e87be675a2304a
a11yster <menonr96@gmail.com>
2026-01-08 23:21:39 +0530

    feat: add enqueue schedule the broker interface

--- brokers/redis/redis.go
+++ brokers/redis/redis.go
@@ -3,12 +3,16 @@ package redis
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-const pollPeriod = time.Second
+const (
+	pollPeriod   = time.Second
+	sortedSetKey = "gotask:ss:%s"
+)
 
 type Broker struct {
 	rdb *redis.Client
@@ -21,6 +25,8 @@ func New() *Broker {
 }
 
 func (b *Broker) Consume(ctx context.Context, work chan []byte, queue string) {
+
+	go b.consumeScheduled(ctx, queue)
 	for {
 		select {
 		case <-ctx.Done():
@@ -41,6 +47,64 @@ func (b *Broker) Consume(ctx context.Context, work chan []byte, queue string) {
 
 func (b *Broker) Enqueue(ctx context.Context, msg []byte, queue string) error {
 	return b.rdb.LPush(ctx, queue, msg).Err()
+}
+
+func (b *Broker) EnqueueScheduled(ctx context.Context, msg []byte, queue string, ts time.Time) error {
+	key := fmt.Sprintf(sortedSetKey, queue)
+	return b.rdb.ZAdd(ctx, key, redis.Z{
+		Score:  float64(ts.UnixNano()),
+		Member: msg,
+	}).Err()
+}
+
+func (b *Broker) consumeScheduled(ctx context.Context, queue string) {
+	ticker := time.NewTicker(pollPeriod)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			b.pumpScheduled(ctx, queue)
+		}
+	}
+}
+
+func (b *Broker) pumpScheduled(ctx context.Context, queue string) {
+	key := fmt.Sprintf(sortedSetKey, queue)
+	now := time.Now().UnixNano()
+
+	err := b.rdb.Watch(ctx, func(tx *redis.Tx) error {
+		tasks, err := tx.ZRevRangeByScore(ctx, key, &redis.ZRangeBy{
+			Min:    "0",
+			Max:    strconv.FormatInt(now, 10),
+			Offset: 0,
+			Count:  1,
+		}).Result()
+
+		if err != nil {
+			return err
+		}
+
+		if len(tasks) == 0 {
+			return nil
+		}
+
+		for _, task := range tasks {
+			if err := b.rdb.LPush(ctx, queue, []byte(task)).Err(); err != nil {
+				return err
+			}
+			if err := tx.ZRem(ctx, key, task).Err(); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("error pumping scheduled jobs: %v\n", err)
+	}
 }
 
 func blpopResult(rs []string) (string, error) {
--- interfaces.go
+++ interfaces.go
@@ -1,10 +1,17 @@
 package gotask
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type Broker interface {
 	// Enqueue pushes a serialized job message onto the queue
 	Enqueue(ctx context.Context, msg []byte, queue string) error
+
+	// EnqueueScheduled accepts a task (msg, queue) and also a timestamp
+	// The job should be enqueued at the particular timestamp.
+	EnqueueScheduled(ctx context.Context, msg []byte, queue string, ts time.Time) error
 
 	// Consume listens on the queue and sends received messages to the work channel.
 	Consume(ctx context.Context, work chan []byte, queue string)
